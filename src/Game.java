805976fcfb02a692757d1b8eba2375779080fc5e
William Vargas <vargas@wustl.edu>
2026-04-02 10:25:50 -0500

    Renamed all variables

--- src/Game.java
+++ src/Game.java
@@ -6,145 +6,143 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Game {
 
 	public static void main(String[] args) {
-		double lv = 0.005;
-		double uv = 0.01;
-		int b = 3;
-		double r = 0.025;
-		int s = 0;
-		int hs = 0;
-		double px = 0.5;
-		double py = 0.5;
-		double ps = 0.01;
-		double[] bx = new double[b];
-		double[] bY = new double[b];
-		double[] bxv = new double[b];
-		double[] bYv = new double[b];
+		double lower_bound = 0.005;
+		double upper_bound = 0.01;
+		int ball_count = 3;
+		double radius = 0.025;
+		int score = 0;
+		int high_score = 0;
+		double player_x = 0.5;
+		double player_y = 0.5;
+		double player_speed = 0.01;
+		double[] ball_x_vals = new double[ball_count];
+		double[] ball_y_vals = new double[ball_count];
+		double[] ball_x_vals_bounded = new double[ball_count];
+		double[] ball_y_vals_bounded = new double[ball_count];
 		
-		for(int i = 0; i < b; i++) {
-			bx[i] = Math.random();
-			bY[i] = Math.random();
-			bxv[i] = Math.random() * (uv - lv) + lv;
-			bYv[i] = Math.random() * (uv - lv) + lv;
+		for(int i = 0; i < ball_count; i++) {
+			ball_x_vals[i] = Math.random();
+			ball_y_vals[i] = Math.random();
+			ball_x_vals_bounded[i] = Math.random() * (upper_bound - lower_bound) + lower_bound;
+			ball_y_vals_bounded[i] = Math.random() * (upper_bound - lower_bound) + lower_bound;
 		}
 		
 		StdDraw.enableDoubleBuffering();
 		
-		long st = System.currentTimeMillis();
-		long dt = System.currentTimeMillis();
+		long score_timer = System.currentTimeMillis();
+		long round_time = System.currentTimeMillis();
 		
 		while (true) {
 			
 			StdDraw.clear();
-			boolean c = false;
-			for(int i = 0; i < b; i++) {
-				
-			
-				bx[i] = bx[i] + bxv[i];
-				bY[i] = bY[i] + bYv[i];
-				if(bx[i] + r > 1 || bx[i] - r < 0) { 
-					bxv[i] = -bxv[i];
+			boolean player_collision = false;
+			for (int i = 0; i < ball_count; i++) {
+				ball_x_vals[i] = ball_x_vals[i] + ball_x_vals_bounded[i];
+				ball_y_vals[i] = ball_y_vals[i] + ball_y_vals_bounded[i];
+				if (ball_x_vals[i] + radius > 1 || ball_x_vals[i] - radius < 0) { 
+					ball_x_vals_bounded[i] = -ball_x_vals_bounded[i];
 				}
-				if(bY[i] + r > 1 || bY[i] - r < 0) { 
-					bYv[i] = -bYv[i];
+				if (ball_y_vals[i] + radius > 1 || ball_y_vals[i] - radius < 0) { 
+					ball_y_vals_bounded[i] = -ball_y_vals_bounded[i];
 				}
-				for(int j = 0; j < b; j++) {
+				for (int j = 0; j < ball_count; j++) {
 					if(i != j) {
-						double d = Math.sqrt(Math.pow(bx[i] - bx[j], 2) + Math.pow(bY[i] - bY[j], 2));
-						if(d < 2 * r) {
-							bxv[i] = -bxv[i];
-							bYv[i] = -bYv[i];
+						double distance_to_ball = Math.sqrt(Math.pow(ball_x_vals[i] - ball_x_vals[j], 2) + Math.pow(ball_y_vals[i] - ball_y_vals[j], 2));
+						if(distance_to_ball < 2 * radius) {
+							ball_x_vals_bounded[i] = -ball_x_vals_bounded[i];
+							ball_y_vals_bounded[i] = -ball_y_vals_bounded[i];
 						}
 					}
 				}
 				
-				double d = Math.sqrt(Math.pow(bx[i] - px, 2) + Math.pow(bY[i] - py, 2));
-				if(d < 2 * r) {
-					c = true;
+				double distance_to_player = Math.sqrt(Math.pow(ball_x_vals[i] - player_x, 2) + Math.pow(ball_y_vals[i] - player_y, 2));
+				if (distance_to_player < 2 * radius) {
+					player_collision = true;
 				}
 			}
 			
-			if(c) {
-				b = 3;
-				for(int i = 0; i < b; i++) {
-					bx[i] = Math.random();
-					bY[i] = Math.random();
-					bxv[i] = Math.random() * (uv - lv) + lv;
-					bYv[i] = Math.random() * (uv - lv) + lv;
-					s = 0;
-					st = System.currentTimeMillis();
-					dt = System.currentTimeMillis();
-					px = 0.5;
-					py = 0.5;
+			if (player_collision) {
+				ball_count = 3;
+				for(int i = 0; i < ball_count; i++) {
+					ball_x_vals[i] = Math.random();
+					ball_y_vals[i] = Math.random();
+					ball_x_vals_bounded[i] = Math.random() * (upper_bound - lower_bound) + lower_bound;
+					ball_y_vals_bounded[i] = Math.random() * (upper_bound - lower_bound) + lower_bound;
+					score = 0;
+					score_timer = System.currentTimeMillis();
+					round_time = System.currentTimeMillis();
+					player_x = 0.5;
+					player_y = 0.5;
 				}
 				
 				
 			}
 			
 			if(StdDraw.isKeyPressed(KeyEvent.VK_W)) {
-				py = py + ps;
+				player_y = player_y + player_speed;
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_S)) {
-				py = py - ps;
+				player_y = player_y - player_speed;
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_A)) {
-				px = px - ps;
+				player_x = player_x - player_speed;
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_D)) {
-				px = px + ps;
+				player_x = player_x + player_speed;
 			}
 			
-			if(px > 1) {
-				px = 1;
+			if(player_x > 1) {
+				player_x = 1;
 			}
-			if(px < 0) {
-				px = 0;
+			if(player_x < 0) {
+				player_x = 0;
 			}
-			if(py > 1) {
-				py = 1;
+			if(player_y > 1) {
+				player_y = 1;
 			}
-			if(py < 0) {
-				py = 0;
+			if(player_y < 0) {
+				player_y = 0;
 			}
 			
 			long now = System.currentTimeMillis();
-			if(now > st + 1000) {
-				s++;
-				if(s > hs) {
-					hs = s;
+			if(now > score_timer + 1000) {
+				score++;
+				if(score > high_score) {
+					high_score = score;
 				}
-				st = now;
+				score_timer = now;
 			}
 			
-			if(now > dt + 10000) {
-				b++;
-				double[] ballXnew = new double[b];
-				double[] ballYnew = new double[b];
-				double[] ballXVnew = new double[b];
-				double[] ballYVnew = new double[b];
-				for(int i = 0; i < b - 1; i++) {
-					ballXnew[i] = bx[i];
-					ballYnew[i] = bY[i];
-					ballXVnew[i] = bxv[i];
-					ballYVnew[i] = bYv[i];
+			if(now > round_time + 10000) {
+				ball_count++;
+				double[] ball_x_vals_new = new double[ball_count];
+				double[] ball_y_vals_new = new double[ball_count];
+				double[] ball_x_vals_bounded_new = new double[ball_count];
+				double[] ball_y_vals_bounded_new = new double[ball_count];
+				for(int i = 0; i < ball_count - 1; i++) {
+					ball_x_vals_new[i] = ball_x_vals[i];
+					ball_y_vals_new[i] = ball_y_vals[i];
+					ball_x_vals_bounded_new[i] = ball_x_vals_bounded[i];
+					ball_y_vals_bounded_new[i] = ball_y_vals_bounded[i];
 				}
-				ballXnew[b-1] = Math.random();
-				ballYnew[b-1] = Math.random();
-				ballXVnew[b-1] = Math.random() * (uv - lv) + lv;
-				ballYVnew[b-1] = Math.random() * (uv - lv) + lv;
-				bx = ballXnew;
-				bY = ballYnew;
-				bxv = ballXVnew;
-				bYv = ballYVnew;
-				dt = now;
+				ball_x_vals_new[ball_count-1] = Math.random();
+				ball_y_vals_new[ball_count-1] = Math.random();
+				ball_x_vals_bounded_new[ball_count-1] = Math.random() * (upper_bound - lower_bound) + lower_bound;
+				ball_y_vals_bounded_new[ball_count-1] = Math.random() * (upper_bound - lower_bound) + lower_bound;
+				ball_x_vals = ball_x_vals_new;
+				ball_y_vals = ball_y_vals_new;
+				ball_x_vals_bounded = ball_x_vals_bounded_new;
+				ball_y_vals_bounded = ball_y_vals_bounded_new;
+				round_time = now;
 			}
 			StdDraw.setPenColor(Color.red);
-			for(int i = 0; i < b; i++) {
-				StdDraw.filledCircle(bx[i], bY[i], r);
+			for(int i = 0; i < ball_count; i++) {
+				StdDraw.filledCircle(ball_x_vals[i], ball_y_vals[i], radius);
 			}
 			
 			StdDraw.setPenColor(Color.black);
-			StdDraw.filledCircle(px, py, r);
-			StdDraw.text(0.5, 0.1, "Score: " + s + " High Score: " + hs);
+			StdDraw.filledCircle(player_x, player_y, radius);
+			StdDraw.text(0.5, 0.1, "Score: " + score + " High Score: " + high_score);
 			
 			StdDraw.show();
 			StdDraw.pause(10);
